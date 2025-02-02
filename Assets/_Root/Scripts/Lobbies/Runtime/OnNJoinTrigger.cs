b9d4a3795119f25b3e8d44cac309de823cc375fa
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-02-02 20:38:10 +0600

    Organaizing everything

---
+++ Assets/_Root/Scripts/Lobbies/Runtime/OnNJoinTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace _Root.Scripts.Lobbies.Runtime
+{
+    public class OnNJoinTrigger : MonoBehaviour
+    {
+        [SerializeField] private PlayerSessionScriptable playerSessionScriptable; 
+
+        private void OnDestroy()
+        {
+            playerSessionScriptable.Reset();
+        }
+    }
+}
---
+++ Assets/_Root/Scripts/Lobbies/Runtime/PlayerSessionScriptable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Unity.Services.Multiplayer;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Root.Scripts.Lobbies.Runtime
+{
+    [CreateAssetMenu(fileName = "PlayerSession", menuName = "Scriptable/Multiplayer/PlayerSession", order = 0)]
+    public class PlayerSessionScriptable : ScriptableObject
+    {
+        private ISession _session;
+        [SerializeField] private List<string> joinedPlayerIds;
+        public UnityEvent<List<string>> onPlayerJoined;
+
+        public void OnJoinedSession(ISession session)
+        {
+            _session = session;
+            session.PlayerJoined -= SessionOnPlayerJoined;
+            session.PlayerJoined += SessionOnPlayerJoined;
+        }
+
+        private void SessionOnPlayerJoined(string id)
+        {
+            joinedPlayerIds.Add(id);
+            onPlayerJoined.Invoke(joinedPlayerIds);
+        }
+
+        public void Reset()
+        {
+            if (_session != null) _session.PlayerJoined -= SessionOnPlayerJoined;
+        }
+    }
+}
